b4c199b5fccfc13d475aadb8242985dcece76a7e
Xinchun Liu <xcliu@ca.ibm.com>
2023-05-29 20:12:25 -0400

    ready

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const { WebClient } = require('@slack/web-api')
 // console.log(Object.keys(vmath))
 // console.log(Object.keys(execa))
 
-
-// slack related
-const MENTIONS = {
-  "ciddread@us.ibm.com": "icp-cicd",
-  "c3cvt3vm@ca.ibm.com": "icp-sert"
+// constants
+const MAP_ACTIONS = {
+  "ciddread@us.ibm.com": {mention: "icp-cicd", notify: 3, cleanup: 4},
+  "c3cvt3vm@ca.ibm.com": {mention: "icp-sert", notify: 4},
+  "unknown@ibm.com": {mention: "xcliu", notify: 4}
 }
+// slack related
 const slack = new WebClient(process.env.SLACK_TOKEN)
 const channel = ref(process.env.SLACK_CHANNEL)
 const subject = ref('Some slack message <@xcliu>')
@@ -29,6 +30,7 @@ const aws_query_ec2 = ref({})
 const aws_query_vpc = ref({})
 const aws_ec2 = ref({})
 const aws_vpc = ref({})
+const clusters_notify = ref([])
 // global flags
 const flag_awscli_working = ref(false)
 const flag_slack_working = ref(false)
@@ -65,7 +67,7 @@ const aws_ec2_clusters = computed(() => Object.values(aws_ec2.value)
     } else {
       instance.spendingenv = "test"
     }
-    console.log(instance)
+    // console.log(instance)
     return instance
   })
   .sort((x,y) => x.name > y.name ? 1 : -1)
@@ -78,17 +80,20 @@ vcore.watchDeep(aws_vpc, () => {
 vcore.watchDeep(aws_ec2, () => {
   console.log(`total ec2 instances: ${Object.keys(aws_ec2.value).length}`)
   setTimeout(() => {
+    const ec2s = []
     aws_ec2_active.value.forEach(instance => {
       const tag_name = instance.Tags.findIndex(tag => tag.Key === "Name")
       if (tag_name !== -1) {
-        console.log(instance.Tags[tag_name].Value)
+        ec2s.push(instance.Tags[tag_name].Value)
       }
     })
+    console.log(ec2s.sort())
   }, 100)
   setTimeout(() => {
     const clusters = Object.entries(aws_ec2_clusters.value).map(([cluster,members]) => ({
       name: cluster,
       owner: members[members.length - 1].owner,
+      launch: members[0].LaunchTime,
       vpc: members[members.length - 1].VpcId,
       type: members[members.length - 1].InstanceType,
       // owner: members.map(i => i.owner).filter((value, index, array) => array.indexOf(value) === index),
@@ -96,8 +101,13 @@ vcore.watchDeep(aws_ec2, () => {
       instances: members.length
     }))
     console.log(clusters)
-    code.value = JSON.stringify(clusters, "", 2)
-    slack.chat.postMessage({type: "mrkdwn", text: text.value, channel: channel.value}).then(() => flag_slack_working.value = true).catch(() => flag_slack_working.value = false)
+    clusters_notify.value = clusters.filter(c => new Date() - new Date(c.launch) > MAP_ACTIONS[c.owner].notify * 60 * 60 * 1000)
+    code.value = JSON.stringify(clusters_notify.value.map(c => Object.assign({}, c, {launch: vcore.useTimeAgo(new Date(c.launch)).value})), "", 2)
+    subject.value = `notifying long run aws clusters`
+    clusters_notify.value.map(c => c.owner).filter((value, index, array) => array.indexOf(value) === index).forEach(owner => subject.value += ` <@${MAP_ACTIONS[owner].mention}> `)
+    slack.chat.postMessage({type: "mrkdwn", text: text.value, channel: channel.value})
+      .then(() => flag_slack_working.value = true)
+      .catch(() => flag_slack_working.value = false)
   }, 200)
 })
 
@@ -200,6 +210,7 @@ async function refresh () {
     flag_awscli_working.value = true
   } catch (e) {
     flag_awscli_working.value = false
+    console.log(execa.commandSync(`cat ~/.aws/*`, {shell: true}).stdout)
     console.error(e)
   }
 }
